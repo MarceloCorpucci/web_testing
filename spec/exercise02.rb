35b8950da865fa309be58127c20f3a4780369bb7
Marcelo Corpucci <mcorpucci@gmail.com>
2017-04-25 16:10:46 -0300

    Exercise 2 with page objects.

--- spec/exercise02.rb
+++ spec/exercise02.rb
@@ -4,30 +4,22 @@ describe "create a new entry", type: :feature do
 
   context "Given I'm logged into the blog" do
     before :each do
-      visit "http://localhost:5000/login"
-      fill_in 'email', with: 'admin1@gmail.com'
-      fill_in 'password', with: 'admin1'
-      find_by_id('btn-submit').click
-      find('#create_post > a').click
+      @login_page = PageObjects::Login.new
+      @login_page.load
+      @home_page = @login_page.login('admin1@gmail.com', 'admin1')
+      @entry_creation_page = @home_page.go_to_create_post
     end
 
     it "should be able to add a new entry in the blog section" do
-      fill_in 'title', with: 'this is my post'
-      fill_in 'body', with: 'this is the post content. See u!'
-      find('#post').click
-
-      expect(page).to have_text("Entry 'this is my post' created successfully.")
+      @entry_creation_page.create
+      expect(@entry_creation_page.notification).to have_text("Entry 'this is my post' created successfully.")
     end
 
     after :each do
-      visit "http://localhost:5000/admin"
-      find('body > div > div.navbar > div > ul:nth-child(2) > li:nth-child(2) > a').click
-      
-      page.all('body > div > table > tbody').each do |tr|
-        if tr.has_text?('this is my post') 
-        	tr.all('td')[1].click_button('Delete record')
-        end
-      end
+      @admin_home_page = PageObjects::AdminHome.new
+      @admin_home_page.load
+      @admin_entry_page = @admin_home_page.go_to_entry
+      @admin_entry_page.delete_most_recent_one
     end
 
   end
